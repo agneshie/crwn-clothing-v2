1c3d52ef528b3b959fee160c5d5bcec14ff1a2e9
agnesnicer <agnes.nicer@gmail.com>
2023-08-01 06:07:11 +0800

    adding react-router-dom createBrowserRouter RouterProvider, Root component, ErrorPage component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,22 @@
-import Directory from "./components/Directory/Directory.component";
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom';
+
+import Root from './routes/Root/Root.component';
+import ErrorPage from './ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />
+  },
+]);
 
 const App = () => {
-  const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
-  
-  
   return (
-    <Directory categories={categories} />
+    <RouterProvider router={router} />
   );
 }
 
